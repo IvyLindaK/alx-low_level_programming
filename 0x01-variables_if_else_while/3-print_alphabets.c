2c3f297e83415f7fd1852574b293e6fb9313086f
IvyLindaK <Lindaivykhasoa@gmail.com>
2022-09-12 19:37:14 -0700

    Lower and upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints aplhabet in lowercase, then uppercase, then a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return(0);
+}
