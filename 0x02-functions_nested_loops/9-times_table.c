bc88ffbadccdd1a6730f0f69dac0c43bf06cdc2b
IvyLindaK <Lindaivykhasoa@gmail.com>
2022-09-15 12:31:04 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,17 +12,17 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
-		
+
 		for (mul = 1;mul <= 9; mul++)
 		{
 			_putchar(',');
 			_putchar(' ');
 			prod = num * mul;
 
-			if(prod <= 9)
+			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod/10) + '0');
+				_putchar((prod / 10) + '0');
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
