2a9174b867a5ddc463155fb427f75ee17aeff7ba
IvyLindaK <Lindaivykhasoa@gmail.com>
2022-09-15 02:22:17 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,11 +11,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		return (1);
+		_putchar('+');
 	}
 	else if (n < 0)
 	{
 		return (-1);
+		_putchar('0');
 	}
 	else 
+	{
 		return (0);
+		_putchar('-');
+	}
 }
