4119e44850da5880bbd49443d203fb348a11781e
IvyLindaK <Lindaivykhasoa@gmail.com>
2022-09-20 13:50:01 -0700

    Length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of a string
+ * @s: Arguments of function
+ * Return: Returns length of string
+ */
+
+int _strlen(char *s)
+{
+	int len;
+	len = strlen(*s);
+	return (len);
+}
