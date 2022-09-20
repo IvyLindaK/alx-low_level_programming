7793b491108198f37869fae706f689632cc03681
IvyLindaK <Lindaivykhasoa@gmail.com>
2022-09-20 14:08:39 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - prints string followed by new line
+ * @str - Argument
+ */
+
+void _puts(char *str)
+{
+	int c;
+
+	for (c = 0; *str != '\0': c++)
+		return (str);
+}
+	
