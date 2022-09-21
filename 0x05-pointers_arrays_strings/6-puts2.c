d8544a24fe9a82fb9a7e314659629bd53dbfc226
IvyLindaK <Lindaivykhasoa@gmail.com>
2022-09-20 20:08:08 -0700

    puts string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,12 +2,13 @@
 
 /**
  * puts2 - prints every other character of a string
- * @str - string argument
+ * @str: string argument
  */
 
 void puts2(char *str)
 {
 	int i = 0;
+
 	while (*(str + i) != '\0')
 	{
 		if (i % 2 == 0)
